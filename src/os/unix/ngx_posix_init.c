002a64beb8c327907984f6dcda1b0f180547969b
noah <noah1999@qq.com>
2024-02-21 15:13:29 +0800

    格式化

--- src/os/unix/ngx_posix_init.c
+++ src/os/unix/ngx_posix_init.c
@@ -4,75 +4,76 @@
  * Copyright (C) Nginx, Inc.
  */
 
-
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <nginx.h>
 
+ngx_int_t ngx_ncpu;        // cpu个数
+ngx_int_t ngx_max_sockets; // 每个进程能打开的最多文件数。
+ngx_uint_t ngx_inherited_nonblocking;
+ngx_uint_t ngx_tcp_nodelay_and_tcp_nopush;
 
-ngx_int_t   ngx_ncpu; //cpu����
-ngx_int_t   ngx_max_sockets;//ÿ�������ܴ򿪵�����ļ����� 
-ngx_uint_t  ngx_inherited_nonblocking;
-ngx_uint_t  ngx_tcp_nodelay_and_tcp_nopush;
+struct rlimit rlmt; // 每个进程能打开的最多文件数。
 
-
-struct rlimit  rlmt; //ÿ�������ܴ򿪵�����ļ����� 
-
-
-
-ngx_os_io_t ngx_os_io = {//�����linux���ұ������ʹ����sendfile������ngx_os_specific_init��ֵngx_os_io = ngx_linux_io;
+ngx_os_io_t ngx_os_io = { // 如果是linux并且编译过程使能了sendfile这里面ngx_os_specific_init赋值ngx_os_io = ngx_linux_io;
     ngx_unix_recv,
     ngx_readv_chain,
     ngx_udp_unix_recv,
     ngx_unix_send,
     ngx_writev_chain,
-    0
-};
+    0};
 
-//����ngx_os_init()��ʼ��ϵͳ��ر��������ڴ�ҳ���Сngx_pagesize,ngx_cacheline_size,���������ngx_max_sockets�ȣ�
+// 调用ngx_os_init()初始化系统相关变量，如内存页面大小ngx_pagesize,ngx_cacheline_size,最大连接数ngx_max_sockets等；
 ngx_int_t
 ngx_os_init(ngx_log_t *log)
 {
-    ngx_uint_t  n;
+    ngx_uint_t n;
 
 #if (NGX_HAVE_OS_SPECIFIC_INIT)
-    if (ngx_os_specific_init(log) != NGX_OK) { //�����linux�������渳ֵngx_os_io = ngx_linux_io;
+    if (ngx_os_specific_init(log) != NGX_OK)
+    { // 如果是linux，这里面赋值ngx_os_io = ngx_linux_io;
         return NGX_ERROR;
     }
 #endif
 
-    if (ngx_init_setproctitle(log) != NGX_OK) {
+    if (ngx_init_setproctitle(log) != NGX_OK)
+    {
         return NGX_ERROR;
     }
 
     /*
-    ����һ����ҳ�Ĵ�С����λΪ�ֽ�(Byte)����ֵΪϵͳ�ķ�ҳ��С����һ�����Ӳ����ҳ��С��ͬ�� 
+    返回一个分页的大小，单位为字节(Byte)。该值为系统的分页大小，不一定会和硬件分页大小相同。
     */
     ngx_pagesize = getpagesize();
     ngx_cacheline_size = NGX_CPU_CACHE_LINE;
 
-    for (n = ngx_pagesize; n >>= 1; ngx_pagesize_shift++) { /* void */ }
+    for (n = ngx_pagesize; n >>= 1; ngx_pagesize_shift++)
+    { /* void */
+    }
 
 #if (NGX_HAVE_SC_NPROCESSORS_ONLN)
-    if (ngx_ncpu == 0) {
-        ngx_ncpu = sysconf(_SC_NPROCESSORS_ONLN); //��ȡϵͳ�п��õ� CPU ����, û�б������ CPU ��ͳ�� ����, ���������Ӻ�û�м����. 
+    if (ngx_ncpu == 0)
+    {
+        ngx_ncpu = sysconf(_SC_NPROCESSORS_ONLN); // 获取系统中可用的 CPU 数量, 没有被激活的 CPU 则不统计 在内, 例如热添加后还没有激活的.
     }
 #endif
 
-    if (ngx_ncpu < 1) {
+    if (ngx_ncpu < 1)
+    {
         ngx_ncpu = 1;
     }
 
     ngx_cpuinfo();
 
-    if (getrlimit(RLIMIT_NOFILE, &rlmt) == -1) { // ÿ�������ܴ򿪵�����ļ����� 
-        
+    if (getrlimit(RLIMIT_NOFILE, &rlmt) == -1)
+    { // 每个进程能打开的最多文件数。
+
         ngx_log_error(NGX_LOG_ALERT, log, errno,
                       "getrlimit(RLIMIT_NOFILE) failed)");
         return NGX_ERROR;
     }
 
-    ngx_max_sockets = (ngx_int_t) rlmt.rlim_cur;
+    ngx_max_sockets = (ngx_int_t)rlmt.rlim_cur;
 
 #if (NGX_HAVE_INHERITED_NONBLOCK || NGX_HAVE_ACCEPT4)
     ngx_inherited_nonblocking = 1;
@@ -85,9 +86,7 @@ ngx_os_init(ngx_log_t *log)
     return NGX_OK;
 }
 
-
-void
-ngx_os_status(ngx_log_t *log)
+void ngx_os_status(ngx_log_t *log)
 {
     ngx_log_error(NGX_LOG_NOTICE, log, 0, NGINX_VER_BUILD);
 
@@ -103,7 +102,6 @@ ngx_os_status(ngx_log_t *log)
                   "getrlimit(RLIMIT_NOFILE): %r:%r",
                   rlmt.rlim_cur, rlmt.rlim_max);
 }
-
 
 #if 0
 
